744af04352b7ee6c2efc2bbad38e2173e7720c39
Marc Güttler <marc.guettler01@gmail.com>
2025-10-16 20:35:07 +0200

    Add Navbar and Menu Button

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,23 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type {Route} from "./+types/home";
+import NavMenu from "../components/NavMenu";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    return [
+        {title: "Nexus"},
+        {name: "description", content: "Welcome to React Router!"},
+    ];
 }
 
 export default function Home() {
-  return <Welcome />;
+    return (
+        <main>
+            <header className="flex justify-between p-4 bg-indigo-800 w-full items-center">
+                <div className="flex flex-row items-center gap-4">
+                    <img src="images/logo.png" alt="Logo" className="h-12 rounded-full"/>
+                    <h1 className="text-2xl">Nexus</h1>
+                </div>
+                <NavMenu/>
+            </header>
+        </main>
+    )
 }
